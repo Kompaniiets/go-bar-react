b7585a47d1d263636cf3ac544392b08c6fd9a66d
Igor Kompaniets <igor.kompaniets.cr@gmail.com>
2018-11-22 17:02:21 +0200

    feature/refactoring Add image uploading ### features - Refactor code

--- src/components/Profile/UploadBarLogo/index.js
+++ src/components/Profile/UploadBarLogo/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import HttpService from '../../../services/httpServices';
+import './style.css';
 
 export default class UploadLogo extends Component {
+    state = {
+        file: null
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        if (!this.state.file) return;
+
+        const formData = new FormData();
+        formData.append('image', this.state.file);
+
+        HttpService.put('users/bars/avatar', formData, {
+            'content-type': 'multipart/form-data'
+        })
+            .then(res => this.props.onProfileUpdate({id: 'avatarUrl', value: res.data.avatarUrl}))
+            .catch();
+    };
+
+    onChange = (e) => this.setState({ file: e.target.files[0] });
+
     render() {
         return (
-            <div className="">
-
+            <div className="avatar-style">
+                <img src={this.props.avatarUrl ? this.props.avatarUrl : ''} alt=""/>
+                <form onSubmit={this.onFormSubmit}>
+                    <input type="file" name="myImage" onChange={this.onChange}/>
+                    <button type="submit" className="btn-style">Upload</button>
+                </form>
             </div>
         )
     }
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,28 +3,78 @@ import ViewProfile from './ViewProfile';
 import UploadLogo from './UploadBarLogo';
 import MapContainer from '../Map';
 import HttpService from '../../services/httpServices';
+import UserModel from '../../models/user';
 import { Auth } from '../../services/AuthService';
 import './style.css';
 
 export default class Profile extends Component {
-    onSubmit = (data, type) => {
-        if (type === 'info') {
-            HttpService.patch('users/me', data)
-                .then(res => Auth.updateStorage(res.data))
-                .catch((err) => console.log('err ', err));
-        } else {
-            HttpService.post('users/locations', data)
-                .then(res => console.log('res ', res))
-                .catch((err) => console.log('err ', err));
+    state = {
+        user: { ...UserModel({}) },
+        error: {
+            hasError: false,
+            errorMessage: ''
         }
+    };
+
+    componentDidMount() {
+        HttpService.get('users/me')
+            .then(res => {
+                this.setState({
+                    user: { ...res.data }
+                });
+            })
+            .catch((err) => this.handleError(err));
+    }
+
+    onSubmit = (data, type) => {
+        // if (type === 'info') {
+        //     HttpService.patch('users/me', data)
+        //         .then(res => Auth.updateStorage(res.data))
+        //         .catch((err) => console.log('err ', err));
+        // } else {
+        //     HttpService.post('users/locations', data)
+        //         .then(res => console.log('res ', res))
+        //         .catch((err) => console.log('err ', err));
+        // }
+    };
+
+    onProfileUpdate = (event) => {
+        this.setState(prevState => ({
+            user: {
+                ...prevState.user,
+                [event.id]: event.value
+            }
+        }));
+    };
+
+    handleError = (err) => {
+        this.setState({
+            error: {
+                hasError: true,
+                errorMessage: err
+            }
+        });
     };
 
     render() {
         const check = Auth.getProfile();
         return (
             <div className="profile-container">
-                <ViewProfile onSubmit={this.onSubmit} isBar={check.isBar}/>
-                {check.isBar ? <UploadLogo/> : ''}
+                <div>
+                    <ViewProfile
+                        onSubmit={this.onSubmit}
+                        user={this.state.user}
+                        error={this.state.error}
+                        onProfileUpdate={this.onProfileUpdate}
+                    />
+                    {check.isBar ?
+                        <UploadLogo
+                            avatarUrl={this.state.user.avatarUrl}
+                            error={this.state.error}
+                            onProfileUpdate={this.onProfileUpdate}
+                        />
+                        : ''}
+                </div>
                 {check.isBar ?
                     <div className="map-wrapper">
                         <MapContainer onSubmit={this.onSubmit}/>
