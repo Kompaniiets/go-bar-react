74c9d7361dffb6fc633e75225df364676bfc9a3d
Igor Kompaniets <igor.kompaniets.cr@gmail.com>
2018-11-28 13:59:52 +0200

    dev Update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,8 @@
 export default {
     version: 'v1',
-    url: process.env.NODE_ENV === 'production' ? 'http://ec2-35-171-166-44.compute-1.amazonaws.com/api/' : `http://localhost:4500/api/`,
-    // google: {
-    //     API_KEY:'AIzaSyAbDDLYtgs_rWiB63Z-htH_vVAcOkIkoaI',
-    //     GOOGLE_API: 'https://maps.google.com/maps/api/geocode/json'
-    // },
+    url: process.env.NODE_ENV === 'production' ? '' : `http://localhost:4500/api/`,
     google: {
-        API_KEY:'AIzaSyC5pl_W_UyB30cjGpvj5lXo-yBMFS1I2dY',
-        GOOGLE_API: 'https://maps.google.com/maps/api/geocode/json'
+        API_KEY:'xxx',
+        GOOGLE_API: 'xxx'
     }
 }
