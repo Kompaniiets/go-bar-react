b04f8572350b1d21901eec743710ab49504c331b
Igor Kompaniets <igor.kompaniets.cr@gmail.com>
2018-11-28 15:37:10 +0200

    dev Update styles

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     version: 'v1',
-    url: process.env.NODE_ENV === 'production' ? '' : `http://localhost:4500/api/`,
+    url: process.env.NODE_ENV === 'production' ? 'xxx' : `http://localhost:4500/api/`,
     google: {
         API_KEY:'xxx',
         GOOGLE_API: 'xxx'
